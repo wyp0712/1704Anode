da69b27ae627d8363ed9a55bc803a48f455eac47
devin-wang <13263262903@163.com>
2019-06-09 21:58:59 +0800

    day3

---
+++ day3-async/async.js
@@ -0,0 +1,9 @@
+const fetch = require('node-fetch');
+
+async function getTitle(url) {
+  let response = await fetch(url);
+  let html = await response.text();
+  return html.match(/<title>([\s\S]+)<\/title>/i)[1];
+}
+getTitle('https://www.npmjs.com/package/node-fetch').then(console.log)
+// "ECMAScript 2017 Language Specification"
